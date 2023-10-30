a67b55aa09ba2d1f206cc950cebb053c243202be
Awanisha <130660671+AwaNisha67@users.noreply.github.com>
2023-10-30 13:43:46 +0530

    Add files via upload

---
+++ Simple integer queue.c
@@ -0,0 +1,109 @@
+/*IMPLEMENT THE OPERATIONS OF SIMPLE QUEUE IN AN INTEGER ARRAY (INSERT, DELETE, PEEK, ISEMPTY, ISFULL)*/
+#include<stdio.h>
+#include <stdlib.h>
+void isEmpty(int i){
+    if(i == -1){
+        printf("\nQueue is empty");
+    }
+    else{
+        printf("\nQueue is not empty yet");
+    }
+    
+}
+int peek(int arr[],int i){
+    if(i == -1){
+        isEmpty(i); 
+        return -1;
+    }
+    return i;
+}
+
+void isFull(int size,int i){
+    if(i == size-1){
+        printf("\nQueue is Fulll");
+        return;
+    }
+    printf("\nQueue is not yet Full");
+}
+int enqueue(int arr[],int size,int value,int i,int *j){
+    if(i == size-1){
+        isFull(size,i);
+        return;
+    }else{
+        if(*j==-1){
+            *j=0;
+        }
+        i++;
+        arr[i]=value;
+    }
+     return i;
+}
+int dequeue(int arr[],int size,int *j){
+    if(*j == -1){
+        isEmpty(*j);
+        return;
+    }
+    (*j)--;
+    
+    return *j;
+}
+void display(int arr[],int size,int i,int j){
+    if (j == -1) {
+        isEmpty(j);
+    }
+    else{
+    int k;
+    for(k=j;k<=i;k++){
+        printf("%d ",arr[k]);
+    }
+        
+    }
+}
+
+void main(){
+    int arr[50],size=5,value,topValue,ch;
+    int i=-1,j=-1;
+    // printf("Enter the size of Your array: ");
+    // scanf("%d",&size);
+    printf("\nPress 1: Insert");
+    printf("\nPress 2: Delete");
+    printf("\nPress 3: Peek");
+    printf("\nPress 4: Empty or not");
+    printf("\nPress 5: Full or not");
+    printf("\nPress 6: Display");
+    printf("\nPress 7: exit");
+    while(1){
+        printf("\nEnter choice: ");
+        scanf("%d",&ch);
+        
+    switch(ch){
+        case 1:printf("\nEnter value to insert: ");
+        scanf("%d",&value);
+        i=enqueue(arr,size,value,i,&j);
+        break;
+        
+        case 2:j=dequeue(arr,size,&j);
+        break;
+        
+        case 3:topValue=peek(arr,i);
+        printf("Value at %d is %d",topValue,arr[topValue]);
+        break;
+        
+        case 4:isEmpty(j);
+        break;
+        
+        case 5:isFull(size,i);
+        break;
+        
+        case 6:display(arr,size,i,j);
+        break;
+        
+        case 7:printf("\n****Code has been terminated****");
+        exit(1);
+        break;
+        
+        default:printf("\nInvalid choice");
+        break;
+    }
+    }
+}
